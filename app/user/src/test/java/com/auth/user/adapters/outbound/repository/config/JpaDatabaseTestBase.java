85a0f511c4e235547ad52e5425014b428e3255b6
Marcus-Nastasi <vinnie.nsts@gmail.com>
2026-05-18 04:45:08 -0300

    feat: user database tests separating config class

---
+++ app/user/src/test/java/com/auth/user/adapters/outbound/repository/config/JpaDatabaseTestBase.java
@@ -0,0 +1,22 @@
+package com.auth.user.adapters.outbound.repository.config;
+
+import com.auth.user.adapters.outbound.repository.UserJpaRepo;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
+import org.springframework.test.context.ActiveProfiles;
+
+@DataJpaTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
+public class JpaDatabaseTestBase {
+
+    @Autowired
+    protected UserJpaRepo userJpaRepo;
+
+    @PersistenceContext
+    protected EntityManager entityManager;
+
+}
